cb633f1ad18502f3680eea56195e7c5a005ef665
atherqureshi <atherqureshi07@gmail.com>
2021-11-06 00:32:08 -0400

    added router for generator path, ported p5js code, still tinkering alogirthm to match local host one

---
+++ src/routes/Generator.tsx
@@ -0,0 +1,15 @@
+import { SocialsDiv } from "../components/SocialsDiv"
+
+import GeneratorDiv from "../components/GeneratorDiv";
+
+export const Generator = () => {
+
+    return <main>
+    <SocialsDiv />
+
+    <h1 className="centerTitle glitch"> GENERATOR </h1>
+
+    <GeneratorDiv />
+
+    </main>
+}
