dba6cbcfdb38a11cbb6ce52bb2e136427d65aeb6
atherqureshi <atherqureshi07@gmail.com>
2021-11-13 12:55:12 -0500

    adding node express server, moved client to client mfolder

---
+++ client/src/components/GeneratorDiv.tsx
@@ -0,0 +1,287 @@
+import React from "react";
+import Sketch from "react-p5";
+import { Typography } from '@mui/material';
+
+const RESOLUTION = 720;
+const CIRCLE_RADIUS = RESOLUTION / 3;
+const CIRCLE_DIAMETER = (RESOLUTION / 3) * 2;
+const SQUARE_SIZE = RESOLUTION / 2;
+
+const get_angle_function_from_string = (value: string, p5: any) => {
+  switch (value) {
+    case "tan":
+      return (angle: any) => p5.tan(angle);
+    case "sin":
+      return (angle: any) => p5.sin(angle);
+    case "cos":
+      return (angle: any) => p5.cos(angle);
+    case "inverse_tan":
+      return (angle: any) => 1 / p5.tan(angle);
+    case "inverse_sin":
+      return (angle: any) => 1 / p5.sin(angle);
+    case "inverse_cos":
+      return (angle: any) => 1 / p5.cos(angle);
+  }
+  return (angle: any) => p5.sin(angle);
+};
+
+class GeneratorDiv extends React.Component<{
+  BACKGROUND_TYPE: number;
+  SHAPE_TYPE: number;
+  SHAPE_BORDER: number;
+  SEED_STRING: string;
+}> {
+  points: any[];
+  r1: number;
+  r2: number;
+  g1: number;
+  g2: number;
+  b1: number;
+  b2: number;
+  mult: any;
+  angle_func_a: Function;
+  angle_func_b: Function;
+  SEED_NUMBER: number;
+  SEED_STRING: string;
+
+  constructor(props: any) {
+    super(props);
+    this.points = [];
+    this.r1 = 0;
+    this.r2 = 0;
+    this.g1 = 0;
+    this.g2 = 0;
+    this.b1 = 0;
+    this.b2 = 0;
+    this.mult = 0;
+    this.angle_func_a = () => {};
+    this.angle_func_b = () => {};
+    var seedrandom = require("seedrandom");
+    this.SEED_STRING = props.SEED_STRING;
+    this.SEED_NUMBER = seedrandom(props.SEED_STRING).int32();
+  }
+
+  // @ts-ignore
+  setup = (p5, canvasParentRef) => {
+    p5.createCanvas(RESOLUTION, RESOLUTION).parent(canvasParentRef);
+    p5.smooth();
+    p5.angleMode(p5.DEGREES);
+    p5.frameRate(160);
+    p5.randomSeed(this.SEED_NUMBER);
+    p5.noiseSeed(this.SEED_NUMBER);
+
+    const NOISE_FACTORS = [0.5, 1, 1.5, 2, 2.5, 3];
+    const REVERSED_NOISE_FACTORS = NOISE_FACTORS.reverse();
+    p5.noiseDetail(p5.random(NOISE_FACTORS), p5.random(REVERSED_NOISE_FACTORS));
+    const ANGLE_FUNC_OPTIONS = [
+      "sin",
+      "cos",
+      "tan",
+      "inverse_sin",
+      "inverse_cos",
+      "inverse_tan",
+    ];
+    const REVERSED_ANGLE_FUNC_OPTIONS = ANGLE_FUNC_OPTIONS.reverse();
+    this.angle_func_a = get_angle_function_from_string(
+      p5.random(ANGLE_FUNC_OPTIONS),
+      p5
+    );
+    this.angle_func_b = get_angle_function_from_string(
+      p5.random(REVERSED_ANGLE_FUNC_OPTIONS),
+      p5
+    );
+
+    const DENSITY = p5.random(500, 1500);
+    var space = p5.width / DENSITY;
+
+    // Initial points in pixel
+    for (var x = 0; x < p5.width; x += space) {
+      for (var y = 0; y < p5.height; y += space) {
+        var p = p5.createVector(x + p5.random(-10, 10), y + p5.random(-10, 10));
+        // @ts-ignore
+        this.points.push(p);
+      }
+    }
+    p5.shuffle(this.points, true);
+    this.r1 = p5.random(255);
+    this.r2 = p5.random(255);
+    this.g1 = p5.random(255);
+    this.g2 = p5.random(255);
+    this.b1 = p5.random(255);
+    this.b2 = p5.random(255);
+    p5.background(0);
+
+    if (this.props.BACKGROUND_TYPE === 1) {
+      p5.background(40);
+      var c1 = p5.color(0);
+      var c2 = p5.color(
+        p5.floor(this.r1 - 60),
+        p5.floor(this.g1 - 60),
+        p5.floor(this.b1 - 60)
+      );
+      var c3 = p5.color(
+        p5.floor(this.r1 - 60),
+        p5.floor(this.g2 - 60),
+        p5.floor(this.b2 - 60)
+      );
+
+      for (let y = 0; y < p5.height; y++) {
+        var n = p5.map(y, 0, p5.height, 0, 1);
+        var selected_color = 0;
+        if (y % 2) {
+          selected_color = c2;
+        } else {
+          selected_color = c3;
+        }
+        let newc = p5.lerpColor(selected_color, c1, n);
+        p5.stroke(newc);
+        p5.line(0, y, p5.width, y);
+      }
+    }
+
+    this.mult = p5.random(0.0005, 0.01);
+
+    p5.fill(15, 25);
+    if (this.props.SHAPE_BORDER === 0) {
+      // draw circle (color, alpha value(transparency))
+      // border around shape if no stroke is on
+      p5.noStroke();
+    }
+    // create shadow for circle
+    p5.drawingContext.shadowOffsetX = 5;
+    p5.drawingContext.shadowOffsetY = -5;
+    p5.drawingContext.shadowBlur = 10;
+    p5.drawingContext.shadowColor = "black";
+
+    // draw shape
+    if (this.props.SHAPE_TYPE === 1) {
+      p5.circle(p5.width / 2, p5.height / 2, CIRCLE_DIAMETER);
+    } else if (this.props.SHAPE_TYPE === 2) {
+      p5.rect(p5.width / 4, p5.height / 4, SQUARE_SIZE, SQUARE_SIZE);
+    }
+    p5.drawingContext.shadowOffsetX = 0;
+    p5.drawingContext.shadowOffsetY = 0;
+    p5.drawingContext.shadowBlur = 0;
+  };
+
+  // @ts-ignore
+  draw = (p5) => {
+    if (p5.frameCount > 1500) {
+      p5.noLoop();
+    }
+
+    p5.noStroke();
+    var max = 0;
+    if (p5.frameCount * 2 <= this.points.length) {
+      max = p5.frameCount * 2;
+    } else {
+      max = this.points.length;
+    }
+
+    for (var i = 0; i < max; i++) {
+      var r = p5.map(this.points[i].x, 0, p5.width, this.r1, this.r2);
+      var g = p5.map(this.points[i].y, 0, p5.height, this.g1, this.g2);
+      var b = p5.map(this.points[i].x, 0, p5.width, this.b1, this.b2);
+
+      var alpha;
+      if (this.props.SHAPE_TYPE === 0) {
+        alpha = p5.map(
+          p5.dist(
+            p5.width / 2,
+            p5.height / 2,
+            this.points[i].x,
+            this.points[i].y
+          ),
+          0,
+          RESOLUTION,
+          700,
+          0
+        );
+      } else if (this.props.SHAPE_TYPE === 1) {
+        alpha = p5.map(
+          p5.dist(
+            p5.width / 2,
+            p5.height / 2,
+            this.points[i].x,
+            this.points[i].y
+          ),
+          0,
+          CIRCLE_RADIUS,
+          700,
+          0
+        );
+      } else if (this.props.SHAPE_TYPE === 2) {
+        alpha = p5.map(
+          p5.dist(
+            p5.width / 2,
+            p5.height / 2,
+            this.points[i].x,
+            this.points[i].y
+          ),
+          0,
+          SQUARE_SIZE,
+          700,
+          0
+        );
+      }
+
+      p5.fill(r, g, b, alpha);
+
+      var angle = p5.map(
+        p5.noise(this.points[i].x * this.mult, this.points[i].y * this.mult),
+        0,
+        1,
+        0,
+        720
+      );
+      this.points[i].add(
+        p5.createVector(this.angle_func_a(angle), this.angle_func_b(angle))
+      );
+
+      if (this.props.SHAPE_TYPE === 0) {
+        p5.ellipse(this.points[i].x, this.points[i].y, 1, p5.random(3));
+      } else if (this.props.SHAPE_TYPE === 1) {
+        if (
+          p5.dist(
+            p5.width / 2,
+            p5.height / 2,
+            this.points[i].x,
+            this.points[i].y
+          ) < CIRCLE_DIAMETER
+        ) {
+          p5.ellipse(this.points[i].x, this.points[i].y, 1, p5.random(3));
+        }
+      } else if (this.props.SHAPE_TYPE === 2) {
+        if (
+          this.points[i].x > SQUARE_SIZE - SQUARE_SIZE / 2 &&
+          this.points[i].x < SQUARE_SIZE + SQUARE_SIZE / 2 &&
+          this.points[i].y > SQUARE_SIZE - SQUARE_SIZE / 2 &&
+          this.points[i].y < SQUARE_SIZE + SQUARE_SIZE / 2
+        ) {
+          p5.ellipse(this.points[i].x, this.points[i].y, 1, p5.random(3));
+        }
+      }
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <Typography align="center">{`Seed: ${this.SEED_STRING}`}</Typography>
+        <br/>
+        <div
+          className="gradient-border"
+          style={{
+            width: "720px",
+            height: "720px",
+            margin: "0 auto",
+          }}
+        >
+          <Sketch setup={this.setup} draw={this.draw} />
+        </div>
+      </>
+    );
+  }
+}
+
+export default GeneratorDiv;
---
+++ client/src/routes/Generator.tsx
@@ -0,0 +1,251 @@
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { SocialsDiv } from "../components/SocialsDiv";
+import GeneratorDiv from "../components/GeneratorDiv";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+
+import * as anchor from "@project-serum/anchor";
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
+import { WalletDialogButton } from "@solana/wallet-adapter-material-ui";
+import { PublicKey } from "@solana/web3.js";
+import { shortenAddress } from "../candy-machine";
+
+export interface GeneratorProps {
+  connection: anchor.web3.Connection;
+}
+
+export const Generator = (props: GeneratorProps) => {
+  const [backgroundType, setBackgroundType] = useState(0);
+  const [shapeType, setShapeType] = useState(1);
+  const [shapeBorder, setShapeBorder] = useState(0);
+  const [seedString, setSeedString] = useState("Solana");
+  const [seedStringError, setSeedStringError] = useState<string | undefined>(
+    undefined
+  );
+  const [disableBorderType, setDisableBorderType] = useState<boolean>(false);
+
+  const [childKey, setChildKey] = useState(1);
+
+  const wallet = useAnchorWallet();
+  const TICKET_TOKEN_PROGRAM_ID = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+
+  const [tickets, setTickets] = useState(0);
+
+  const validateSeedString = (value: string) => {
+    if (value.length > 16) {
+      setSeedStringError(
+        "Seed String must equal 16 characters or less characters"
+      );
+    } else {
+      setSeedString(value);
+      setSeedStringError(undefined);
+    }
+  };
+
+  useEffect(() => {
+    if (shapeType === 0 || backgroundType === 0) {
+      setShapeBorder(0);
+      setDisableBorderType(true);
+    } else {
+      setDisableBorderType(false);
+    }
+  }, [shapeType, backgroundType]);
+
+  useEffect(() => {
+    setChildKey((prev) => prev + 1);
+  }, [shapeType, backgroundType, shapeBorder]);
+
+  useEffect(() => {
+    if (wallet) {
+      props.connection
+        .getTokenAccountsByOwner(wallet?.publicKey, {
+          programId: new PublicKey(TICKET_TOKEN_PROGRAM_ID),
+        })
+          .then(async (result) => {
+            // loop through public keys, check if they were already created in db
+            console.log(result.value[0].pubkey.toBase58());
+            const response = await fetch('/api/hello');
+            const body = await response.json()
+            console.log(body);
+            setTickets(result.value.length);
+          }
+        );
+    }
+  }, [wallet, props.connection]);
+
+  const SelectItem = styled(Select)(({ theme }) => ({
+    padding: 2,
+    textAlign: "center",
+    fontFamily: "Orbitron",
+  }));
+
+  return (
+    <main>
+      <SocialsDiv />
+
+      <h1 className="centerTitle glitch"> GENERATOR </h1>
+
+      <Box
+        component="form"
+        autoComplete="off"
+        noValidate
+        sx={{
+          width: 600,
+          bgcolor: "#4834d4",
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+          margin: "0 auto",
+          opacity: "0.8",
+          border: "20px solid black",
+          padding: 3,
+          borderRadius: 1,
+          boxShadow: 1,
+        }}
+      >
+        <Stack spacing={3}>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Background</InputLabel>
+            <SelectItem
+              variant="filled"
+              autoWidth
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={backgroundType}
+              label="Background"
+              onChange={(event) => {
+                setBackgroundType(event.target.value as number);
+              }}
+            >
+              <MenuItem value={0}>Black</MenuItem>
+              <MenuItem value={1}>Gradient</MenuItem>
+            </SelectItem>
+          </FormControl>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Shape Type</InputLabel>
+            <SelectItem
+              variant="filled"
+              autoWidth
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={shapeType}
+              label="Shape Type"
+              onChange={(event) => {
+                setShapeType(event.target.value as number);
+              }}
+            >
+              <MenuItem value={0}>None</MenuItem>
+              <MenuItem value={1}>Circle</MenuItem>
+              <MenuItem value={2}>Square</MenuItem>
+            </SelectItem>
+          </FormControl>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Shape Border</InputLabel>
+            <SelectItem
+              variant="filled"
+              autoWidth
+              disabled={disableBorderType}
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={shapeBorder}
+              label="Shape Border"
+              onChange={(event) => {
+                setShapeBorder(event.target.value as number);
+              }}
+            >
+              <MenuItem value={0}>No Border</MenuItem>
+              <MenuItem value={1}>Border</MenuItem>
+            </SelectItem>
+          </FormControl>
+          <FormControl>
+            <TextField
+              sx={{
+                textAlign: "center",
+                padding: 2,
+                fontFamily: "Orbitron",
+              }}
+              label="Seed String"
+              variant="outlined"
+              error={seedStringError !== undefined}
+              helperText={seedStringError}
+              id="outlined-multiline-flexible"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              value={seedString}
+              onChange={(event) => {
+                validateSeedString(event.target.value);
+              }}
+            />
+            <Button
+              variant="contained"
+              onClick={() => {
+                console.log("Check if seed string has been minted");
+              }}
+            >
+              {" "}
+              IS TAKEN?{" "}
+            </Button>
+          </FormControl>
+        </Stack>
+      </Box>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ padding: "20px" }}
+      >
+        <Button
+          sx={{ margin: "0 auto" }}
+          variant="outlined"
+          size="large"
+          onClick={() => {
+            setChildKey((prev) => prev + 1);
+          }}
+        >
+          GENERATE
+        </Button>
+        {!wallet ? (
+          <WalletDialogButton variant="contained" size="large">
+            Connect Wallet
+          </WalletDialogButton>
+        ) : (
+          <div>
+            <p> Wallet: {shortenAddress(wallet.publicKey.toBase58())} </p>
+            <p> Tickets: {tickets} </p>
+            <Button
+              variant="contained"
+              size="large"
+              onClick={() => {
+                // create loading overlay
+                // POST request to backend
+                // send pubkey of wallet + seed string, and parameters (background, shape, border etc)
+                // then, remove overlay, and refresh ticket count (query blockchain again)
+              }}
+            >
+              SUBMIT TICKET
+            </Button>
+          </div>
+        )}
+      </Stack>
+
+      {childKey !== 1 ? (
+        <div key={childKey}>
+          <GeneratorDiv
+            BACKGROUND_TYPE={backgroundType}
+            SHAPE_TYPE={shapeType}
+            SHAPE_BORDER={shapeBorder}
+            SEED_STRING={seedString}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
+    </main>
+  );
+};
